b17e68c9a435133d100aa47311217367b827a412
刘红 <liuhong1.happy@163.com>
2015-07-06 12:51:26 +0800

    Create code.js

---
+++ app/scripts/controllers/code.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:CodeCtrl
+ * @description
+ * # CodeCtrl
+ * Controller of the angularApp
+ */
+angularApp.controller('CodeCtrl', ["$scope","config", function ($scope,config) {
+    var env = config.envirement;
+    $scope.gitlab = config[env].hrefs.code;   
+    
+    var resizeFunc = function(){
+            var width = $($window).width();
+            var height = $($window).height();
+            $scope.gitlab_width = width;
+            $scope.gitlab_height  = height-140;
+            $scope.$apply();
+    }
+    
+    $timeout(function(){
+        resizeFunc();
+    },10);
+     
+    $window.onresize =resizeFunc;
+}]);
