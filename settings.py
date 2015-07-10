64af28e3ea0d8e48e190ab1f67fb4e8a63809b91
刘红 <liuhong1.happy@163.com>
2015-07-10 15:58:37 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,17 @@
 from tornado.options import define, options
 import asyncmongo
-
+# 基本链接信息
 define("db_client", default=None, help="mongodb client", type=asyncmongo.client.Client)
 define("etcd_client", default=None, help="etcd client", type=object)
 define("docker_client", default=None, help="docker client", type=object)
 define("mq_connection", default=None, help="amqp connection", type=object)
+# 常用枚举变量
+define("service_status", default={
+        "created":0,
+        "running":1,
+        "success":2
+    }, help="service status", type=dict)
+
 
 SITE_URL = 'http://127.0.0.1:8888'
 TORNADO_PORT = 8888
@@ -25,3 +32,5 @@ MQ_HOST = '127.0.0.1'
 MQ_HEARTBEAT = 30
 MQ_USERNAME = 'admin'
 MQ_PASSWORD = 'testpass'
+
+
