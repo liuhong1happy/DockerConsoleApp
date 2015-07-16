603bbdaca6a30e81a1e1ef6216b143f996d92ad4
刘红 <liuhong1.happy@163.com>
2015-07-16 12:50:24 +0800

    Update consumers.py

--- consumers.py
+++ consumers.py
@@ -1,3 +1,65 @@
-from stormed import Connection, Message
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import tornado.ioloop
+import tornado.web
+from tornado.options import define, options
+from tornado import gen
+import tornado.httpserver
+import os.path
+import settings
+from util.db import init_db
+from util.discover import init_etcd
+from util.dockerclient import init_docker
+from util.rabbitmq import init_amqp
+
+define("port", default=settings.TORNADO_PORT, help="run on the given port", type=int)
+define("ioloop",default=None,help="global ioloop instance",type=object)
+
+def create_service(self,ch, method, properties, body):
+    print " [x] %r:%r" % (method.routing_key, body,)
+
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        self.init_service()
+        self.init_consumer()
+        handlers = []
+        settings = dict(
+            blog_title=u"Docker中文翻译社区",
+            template_path=os.path.join(os.path.dirname(__file__), "dist"),
+            static_path=os.path.join(os.path.dirname(__file__), "dist"),
+            xsrf_cookies=True,
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            login_url="/login",
+            debug=True
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+    
+    def init_service(self):
+        init_db()
+        init_etcd()
+        init_docker()
+        init_amqp()
+    
+    def init_consumer(self):
+        conn = options.mq_connection
+        ch = conn.chanel()
+        channel.consume(create_service,
+                      queue='create_service',
+                      no_ack=True)
+        channel.start_consuming()
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    
+    ioloop_instance = tornado.ioloop.IOLoop.instance()
+    options.ioloop = ioloop_instance
+    ioloop_instance.start()
+
+if __name__ == "__main__":
+    main()
 
 
