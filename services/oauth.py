bd8a1b527b8989ff5b849b89b5a6cfb890029e30
刘红 <liuhong1.happy@163.com>
2015-07-21 13:00:39 +0800

    Update oauth.py

--- services/oauth.py
+++ services/oauth.py
@@ -1,1 +1,13 @@
+form models.user import UserModel
+import tornado.gen
 
+class OAuthService():
+    m_user = UserModel()
+    
+    def __init__(self):
+        pass
+      
+    @tornado.gen.engine
+    def update_gitlab_token(self,user_id,token=None,callback=None):
+        user = yield tornado.gen.Task(m_user.update,{"_id":user_id},{"$set":{"gitlab_token":token}})
+        callback(user)
