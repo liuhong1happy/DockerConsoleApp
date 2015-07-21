b152029a7e2e91ad28fc6ee65f4a5b2dd5b02e36
刘红 <liuhong1.happy@163.com>
2015-07-21 09:06:03 +0800

    Update oauth.py

--- views/oauth.py
+++ views/oauth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from services.service import ServiceService
+from services.user import UserService
 import tornado.web
 import tornado.gen
 import tornado.escape
@@ -8,36 +8,15 @@ import json
 from util.rabbitmq import send_message
 from stormed import Message
 import settings
+from views import AsyncBashHandler
 
-    
-class GitLabOAuthHandler(tornado.web.RequestHandler):
+class GitLabOAuthHandler(AsyncBashHandler):
     s_oauth = OAuthService()
     
-    @tornado.web.asynchronous
     @tornado.gen.engine
-    def post(self):
-        spec_type = self.get_argument("spec_type","name")
-        spec_text =  self.get_argument("spec_text","")
-        page_index =int(self.get_argument("page_index",0))
-        page_size =int(self.get_argument("page_size",20))
-        spec ={}
-        spec[spec_type]={ '$regex' : spec_text}
-        fields={
-            "name":True,
-            "user":True,
-            "image":True,
-            "code":True,
-            "status":True,
-            "logs":True,
-            "update_time":True,
-            'create_time':True
-        }
-
-        services =yield tornado.gen.Task(self.s_service.get_list,spec,fields=fields,page_index=page_index,page_size=page_size)
-        _json = ""
-        if not services:
-            _json =json.dumps({"status":"error","error_code":404})
-        else:
-            _json = json.dumps({"status":"success","data":services})
-        self.write(_json)
-        self.finish()
+    def _post_(self):
+        access_token = self.get_argument('access_token')
+        gitlab_user = self.get_argument('user')
+        current_user = self.current_user.get('_id','admin')
+        
+        user = yield tornado.gen.Task(self.s_oauth.update,current_user,{} )
