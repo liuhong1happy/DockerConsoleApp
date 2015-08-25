c4dc01b760b93a6dfd9852f794e443f4b7bf76ac
刘红 <liuhong1.happy@163.com>
2015-08-25 11:20:55 +0800

    Update application.py

--- views/application.py
+++ views/application.py
@@ -93,3 +93,49 @@ class ApplicationsHandler(AsyncBaseHandler):
             self.render_error(error_code=404,msg="not data")
         else:
             self.write_result(data=applications)
+            
+class ApplicationAccessHandler(AsyncBaseHandler):
+    s_application = ApplicationService()
+    @gen.coroutine
+    def _get_(self):
+        access_type = self.get_argument("type",None)
+        container_id = self.get_argument("id",None)
+        access_content = self.get_argument("content","")
+        # 从数据库获取，切记不要对外公开
+        # container_host = self.get_argument("host",None)
+        user_id = str(self.current_user.get("_id",None))
+        user_name = str(self.current_user.get("name",None))
+        create_time = time.time()
+
+        # 数据库操作
+        accessData = {}
+        accessData["access_type"] = access_type
+        accessData["container_id"] = container_id
+        accessData["container_host"] = container_host
+        accessData["access_content"] = container_content
+        accessData["user_id"] = user_id
+        accessData["status"] = 'start'
+        accessData["logs"] = [
+          {
+            "create_time":create_time,
+            "info":"started access application:"+container_id+",it is hosted in "+container_host,
+            "user_id":user_id
+          }
+        ]
+        result= yield self.s_application_access.access_application(accessData)
+        # 加入队列
+        msg = Message( json.dumps({
+            "access_type":access_type,
+            "container_id":container_id,
+            "container_host":container_host,
+            "access_content":access_content,
+            "user_id":user_id,
+            "user_name":user_name,
+            "reply_to":'access_logs'
+        }))
+        send_message(msg,settings.ACCESS_APPLICATION_EXCHANGE,settings.ACCESS_APPLICATION_ROUTING)
+        if result is None:
+            self.render_error(error_code=404,msg="not data")
+        else:
+            accessData["_id"] = str(result)
+            self.write_result(data=accessData)
