badf53a591e43be0c90a5b78a7387bcd24786408
刘红 <liuhong1.happy@163.com>
2015-09-09 12:54:51 +0800

    Update applications.js

--- app/scripts/factories/applications.js
+++ app/scripts/factories/applications.js
@@ -1,1 +1,16 @@
-
+angularApp.factory('ApplicationRun',["$resource",function($resource){
+    return $resource('/api/application/run',null,{
+        "run":{
+            method:"post",
+            isArray:false
+        }
+    });
+}]);
+angularApp.factory('ApplicationInfo',["$resource",function($resource){
+    return $resource('/api/application/info',null,{
+        "info":{
+            method:"post",
+            isArray:false
+        }
+    });
+}]);
