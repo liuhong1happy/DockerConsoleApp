e9f06e6f4c5966665b72c385b231310ea2a1eed1
刘红 <liuhong1.happy@163.com>
2015-09-09 12:51:21 +0800

    Update Service.js

--- app/scripts/factories/Service.js
+++ app/scripts/factories/Service.js
@@ -1,3 +1,18 @@
+angularApp.factory('Services',["$resource",function($resource){
+    return $resource('/api/services',{},{
+        read:{
+            method:"GET",
+            isArray:false
+        }
+    });
+}]);
+angularApp.factory('Service',["$resource",function($resource){
 
-
-
+    return $resource('/api/service',{
+    },{
+        "submit":{
+            method:"POST",
+            isArray:false
+        }
+    });
+}]);
