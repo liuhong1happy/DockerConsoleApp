a23a06d591699e6076626d137426f776de27f402
刘红 <liuhong1.happy@163.com>
2015-09-09 12:54:36 +0800

    Update service.js

--- app/scripts/controllers/service.js
+++ app/scripts/controllers/service.js
@@ -138,20 +138,3 @@ function ($scope,config,Services,Service,ServiceInfo,ApplicationRun,ApplicationI
             }
     };
 }]);
-
-angularApp.factory('ApplicationRun',["$resource",function($resource){
-    return $resource('/api/application/run',null,{
-        "run":{
-            method:"post",
-            isArray:false
-        }
-    });
-}]);
-angularApp.factory('ApplicationInfo',["$resource",function($resource){
-    return $resource('/api/application/info',null,{
-        "info":{
-            method:"post",
-            isArray:false
-        }
-    });
-}]);
