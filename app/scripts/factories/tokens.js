eb0ad0212244992bacd755a7deab8d3b22e31ccd
刘红 <liuhong1.happy@163.com>
2015-09-09 12:54:11 +0800

    Update tokens.js

--- app/scripts/factories/tokens.js
+++ app/scripts/factories/tokens.js
@@ -1,1 +1,17 @@
-
+angularApp.factory('GitLabToken',["$resource",function($resource){
+    return $resource('/api/gitlab/token',null,{
+        "getToken":{
+            method:"get",
+            isArray:false
+        }
+    });
+}]);
+      
+angularApp.factory('GitLabRefresh',["$resource",function($resource){
+    return $resource('/api/gitlab/refresh',null,{
+        "refresh":{
+            method:"get",
+            isArray:false
+        }
+    });
+}]);
